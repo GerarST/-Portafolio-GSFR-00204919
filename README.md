619b5a2afd1ed11608162790648cf8cd8ead194c
GerarST <gerarst@GerarST.localdomain>
2020-01-10 16:09:27 -0600

    Modify: readme.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # -Portafolio-GSFR-00204919
 Repositorio de trabajo para la materia de Programación de Estructuras Dinámicas.
+
+##Título de prueba 2
+
+###Título de prueba 3
+
+*Primer elemento de prueba
+*Segundo elemento de prueba
+*Tercer elemento de prueba
---
+++ Segundo Repositorio/Portafolio.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+     int ma1=0;
+     int n=0,m=0;
+     int matriz1[2][2];
+     int matriz2[2][2];
+ 
+       for(int i = 0; i < 2; i++){
+        cout << "NumeroF ["<<i + 1<<"] " << endl;
+        for(int j = 0; j < 2; j++){
+            cout << "NumeroC  ["<<j + 1<<"]: "; cin >> matriz1[i][j];
+        }
+    }
+   
+}
